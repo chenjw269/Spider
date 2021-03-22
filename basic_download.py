977c4d7a499a44c8d66e771d50fa13749ccf23a8
chenjw <1287144748@qq.com>
2021-03-22 08:20:27 +0800

    .

--- basic_download.py
+++ basic_download.py
@@ -1,10 +1,15 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bf
+from urllib.request import urlretrieve
 # 请求获取HTML资源
 html = urlopen("http://www.baidu.com/")
-# 解析html
+# 用BeautifulSoup解析html
 obj = bf(html.read(),'html.parser')
-# 使用find_all函数获取所有图片
-pic_info = obj.find_all('img')
-for i in pic_info:
-    print(i)
+# 从标签head、title里提取标题
+title = obj.head.title
+# 只提取logo图片的信息
+logo_pic_info = obj.find_all('img',class_="index-logo-src")
+# 提取logo图片的链接
+logo_url = "https:"+logo_pic_info[0]['src']
+# 使用urlretrieve下载图片
+urlretrieve(logo_url, 'logo.png')
